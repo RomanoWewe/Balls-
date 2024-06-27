e5f48f213b80b87de4f5a409569795257230cda5
ZiHEd <140422175+ZiHEd@users.noreply.github.com>
2024-06-27 22:26:56 +0200

    update

--- Code/scripts/Bridge.gd
+++ Code/scripts/Bridge.gd
@@ -2,14 +2,14 @@ extends AnimatableBody3D
 class_name Bridge
 
 @export var debris_scene :PackedScene
-@export_range(0,4) var weight_to_break
+@export_range(0,4) var weight_to_break := 4
 var weight_current := 0
-
+@export var linked_objects :Array[Node3D] = []
 
 func _on_area_3d_body_entered(body):
 	if body is Minecart:
 		weight_current+=body.fill_level
-		if weight_current >=weight_to_break:
+		if weight_current >= weight_to_break:
 			destroy()
 
 
@@ -19,6 +19,8 @@ func _on_area_3d_body_exited(body):
 
 func destroy():
 	queue_free()
+	for obj in linked_objects:
+		obj.queue_free()
 	if debris_scene!=null:
 		var instance = debris_scene.instantiate()
 		instance.position = position
